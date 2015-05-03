d85a12e73717caa85b2506b5859584d6ea5ed0a0
Joe Wilson <joe.wlsn@gmail.com>
2015-05-03 15:51:37 -0700

    Adds a view to search and display results

--- imsearch/imapp/views.py
+++ imsearch/imapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from imapp.forms import SearchForm
+from imlib.imgursearch import ImgurVideoSearch
 
 
 def search_input(request):
     search_form = SearchForm()
     return render(request, 'search.html', {'search_form': search_form})
+
+
+def search_results(request):
+    if request.method == 'POST':
+        search_string = request.POST['search_query']
+        iv_search = ImgurVideoSearch()
+        iv_results = iv_search.search(search_string)
+        video_data = iv_results.get('data')
+        return render(request, 'results.html', {'video_data': video_data})

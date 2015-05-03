11fdc28fee533864cca5a29ed204e8ab610b66b2
Joe Wilson <joe.wlsn@gmail.com>
2015-05-03 16:11:20 -0700

    Fixes a path issue in get_config.py

--- imsearch/imlib/get_config.py
+++ imsearch/imlib/get_config.py
@@ -3,7 +3,7 @@ from ConfigParser import RawConfigParser
 
 
 def get_config(cfile, section, item):
-    thisdir = path.dirname(path.dirname(__file__))
+    thisdir = path.dirname(__file__)
     cfgfile = path.join(thisdir, cfile)
     parser = RawConfigParser()
     parser.read(cfgfile)

d0bb22daece36969de9f6ce5369cd3ab35de07f3
Joe Wilson <joe.wlsn@gmail.com>
2015-05-03 15:03:38 -0700

    Adds a landing page url that points to search input

--- imsearch/imsearch/urls.py
+++ imsearch/imsearch/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# from django.contrib import admin
+from imapp import views as imapp_views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'imsearch.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^$', imapp_views.search_input,
+                           name='search_screen'),)

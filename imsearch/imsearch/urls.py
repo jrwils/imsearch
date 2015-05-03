91750db6ed47c64bd441c4d60ca2f7f0984b93fb
Joe Wilson <joe.wlsn@gmail.com>
2015-05-03 16:08:01 -0700

    Adds search results url and form action

--- imsearch/imsearch/urls.py
+++ imsearch/imsearch/urls.py
@@ -4,4 +4,6 @@ from imapp import views as imapp_views
 
 urlpatterns = patterns('',
                        url(r'^$', imapp_views.search_input,
-                           name='search_screen'),)
+                           name='search_screen'),
+                       url(r'^$', imapp_views.search_results,
+                           name='search_results'),)

2345b9d180cea91e503148e15589ad2ccbe2ded1
Joe Wilson <joe.wlsn@gmail.com>
2015-05-03 16:10:35 -0700

    Adds a redirect if the results url does not receive POST data

--- imsearch/imapp/views.py
+++ imsearch/imapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from imapp.forms import SearchForm
 from imlib.imgursearch import ImgurVideoSearch
 
@@ -15,3 +16,4 @@ def search_results(request):
         iv_results = iv_search.search(search_string)
         video_data = iv_results.get('data')
         return render(request, 'results.html', {'video_data': video_data})
+    return HttpResponseRedirect(reverse('search_screen'))

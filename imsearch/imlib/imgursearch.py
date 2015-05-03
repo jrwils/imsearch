b845db14129c89cd11b45775dd96b8b593cf3e8b
Joe Wilson <joe.wlsn@gmail.com>
2015-05-03 14:39:46 -0700

    Adds an ImgurVideoSearch class for searching the Imgur API

---
+++ imsearch/imlib/imgursearch.py
@@ -0,0 +1,17 @@
+import requests
+from get_config import get_config
+
+
+class ImgurVideoSearch(object):
+    def __init__(self):
+        self.api_url = 'https://api.imgur.com/3/gallery/search/time/'
+        self.imgur_key = get_config('imlib.cfg', 'api_keys', 'imgur')
+        self.headers = {'Authorization': 'Client-ID ' + self.imgur_key}
+
+    def search(self, srch_str):
+        srch_params = {'q_all': srch_str,
+                       'q_type': 'anigif'}
+        r = requests.get(self.api_url,
+                         headers=self.headers,
+                         params=srch_params)
+        return r.json()

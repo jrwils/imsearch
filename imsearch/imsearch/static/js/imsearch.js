8e31e7cecc5f96644d5755fcd9b76298432c5229
Joe Wilson <joe.wlsn@gmail.com>
2015-05-04 16:41:07 -0700

    Adds a function to set the current clip number in pagination

--- imsearch/imsearch/static/js/imsearch.js
+++ imsearch/imsearch/static/js/imsearch.js
@@ -1,8 +1,19 @@
+function setCurrentNum(clipNum) {
+    $("#video_num").html(clipNum); 
+}
+
 function nextClip() {
     var currentDescTable = $(".desc_table").filter(":visible")[0];
     var currentTableNum = currentDescTable.id.replace("desc_table_", "");
     var nextTableNum = parseInt(currentTableNum) + 1;
     var nextDescTable = $("#desc_table_" + nextTableNum.toString())[0];
+
+    var clipInp = $("#clip_" + nextTableNum.toString());
+    var clipSrc = clipInp[0].value;
+    var vidFrame = $("#video_embed");   
+
     currentDescTable.style.display = "none";
     nextDescTable.style.display = "";
+    vidFrame[0].src = clipSrc;
+    setCurrentNum((nextTableNum + 1).toString());
 }

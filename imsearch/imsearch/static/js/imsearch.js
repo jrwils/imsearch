50a8c6c240e2894206070e8801380ac205f68c81
Joe Wilson <joe.wlsn@gmail.com>
2015-05-04 17:24:19 -0700

    Adds a showNavButtons js function to show the appropriate navigation buttons depending on current video

--- imsearch/imsearch/static/js/imsearch.js
+++ imsearch/imsearch/static/js/imsearch.js
@@ -2,8 +2,27 @@ function setCurrentNum(clipNum) {
     $("#video_num").html(clipNum); 
 }
 
+function showNavButtons(tableNum) {
+    // we don't want to show the 'previous nav button for the first video
+    // or the 'next' nav button for the last video
+    var prevButton = $("#prev_button")[0];
+    var nextButton = $("#next_button")[0];
+    var numVideos = $(".desc_table").length;
+    if (tableNum == 0) {
+        prevButton.style.display = "none";
+        nextButton.style.display = "";
+    } else if (tableNum == (numVideos - 1)) {
+        prevButton.style.display = "";
+        nextButton.style.display = "none";
+    } else {
+        prevButton.style.display = "";
+        nextButton.style.display = "";
+    }
+}
+
 function nextClip(direction) {
     var currentDescTable = $(".desc_table").filter(":visible")[0];
+    var currentTableNum = currentDescTable.id.replace("desc_table_", "");
     var currentTableNum = currentDescTable.id.replace("desc_table_", "");
 
     if (direction=='forward') {
@@ -11,7 +30,7 @@ function nextClip(direction) {
     } else if (direction=='backward') {
         var nextTableNum = parseInt(currentTableNum) - 1;
     } else {
-        // return if no direction given
+        // return if no valid direction given
         return
     }
     var nextDescTable = $("#desc_table_" + nextTableNum.toString())[0];
@@ -24,4 +43,6 @@ function nextClip(direction) {
     nextDescTable.style.display = "";
     vidFrame[0].src = clipSrc;
     setCurrentNum((nextTableNum + 1).toString());
+
+    showNavButtons(nextTableNum);
 }
